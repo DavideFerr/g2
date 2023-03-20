6070fe8e668a953855cc514cf3e872f7bc29b0d8
DavideFerr <93709164+DavideFerr@users.noreply.github.com>
2023-03-20 09:08:06 +0100

    Create ContaINani.java

---
+++ Laboratorio10/ContaINani.java
@@ -0,0 +1,9 @@
+public class ContaINani extends Thread{
+    public void run(){
+        setName("settenani");
+        System.out.println(Thread.currentThread().getName());
+        for(int i=0; i<7; i++){
+            System.out.print((i+1)+" ");
+        }
+    }
+}
